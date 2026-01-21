e5965ee53df832ba997c7b4e7bb221b86092ae45
Michail <mrjazzyk+github1@gmail.com>
2026-01-21 00:46:54 -0800

    Добавлено приведение числовых типов данных

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -21,8 +21,26 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     pd.DataFrame
         Преобразованный DataFrame.
     """
+    
     df_transformed = df.copy()
 
-    
+    # Список колонок, которые должны быть числовыми
+    numeric_columns = [
+        "age",
+        "exercise_hours_per_week",
+        "sleep_hours_per_night",
+        "daily_active_minutes_instagram",
+        "sessions_per_day",
+        "followers_count",
+        "following_count",
+        "user_engagement_score",
+    ]
+
+    for column in numeric_columns:
+        if column in df_transformed.columns:
+            df_transformed[column] = pd.to_numeric(
+                df_transformed[column],
+                errors="coerce"
+            )
 
     return df_transformed
